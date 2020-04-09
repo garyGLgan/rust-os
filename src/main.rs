1608d481f2f59d1166df63c057b41fd1e87786a9
Gary Gan <garyglgan@gmail.com>
2020-04-09 17:09:00 +0800

    print hellow world

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 #![no_main]
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop{}
-}
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_hello_world();
+    loop{}
+}
+
