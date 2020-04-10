9302d7e5ed3c95fb69f3248dd2a921b6af4000a9
gary.gan <garyglgan@gmail.com>
2020-04-10 09:16:28 +0800

    add test cases

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,28 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use ggos::println;
+use ggos::{println, serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop{}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop{}
-}
-
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     ggos::test_panic_handler(info);
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println...");
+    println!("test_println output");
+    serial_println!("[Ok]");
 }
