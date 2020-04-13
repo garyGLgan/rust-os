55af0e0fc8f00121e9db9ec6ffd8df3fc02f8440
gary.gan <garyglgan@gmail.com>
2020-04-13 22:18:11 +0800

    use offset page table

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,19 @@ use ggos::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use ggos::memory::translate_addr;
-    use x86_64::{structures::paging::PageTable, VirtAddr};
+    use ggos::memory;
+    use x86_64::{
+        structures::paging::{MapperAllSizes, PageTable},
+        VirtAddr,
+    };
 
     println!("Hello World{}", "!");
 
     ggos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+
+    let mapper = unsafe { memory::init(phys_mem_offset) };
     let addresses = [
         0xb8000,
         0x201008,
@@ -28,7 +33,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = unsafe { mapper.translate_addr(virt) };
         println!("{:?} -> {:?}", virt, phys);
     }
 
