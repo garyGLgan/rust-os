e51560dbf33a2671b0cd35213b7d4de9985d7a25
gary.gan <garyglgan@gmail.com>
2020-04-13 21:42:41 +0800

    translate address

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use ggos::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use ggos::memory::active_level_4_table;
+    use ggos::memory::translate_addr;
     use x86_64::{structures::paging::PageTable, VirtAddr};
 
     println!("Hello World{}", "!");
@@ -19,44 +19,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     ggos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table: &mut PageTable = unsafe { active_level_4_table(phys_mem_offset) };
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 Entry {}: {:?}", i, entry);
-
-                    let phys = entry.frame().unwrap().start_address();
-                    let virt = phys.as_u64() + boot_info.physical_memory_offset;
-                    let ptr = VirtAddr::new(virt).as_mut_ptr();
-                    let l2_table: &PageTable = unsafe { &*ptr };
-                    for (i, entry) in l2_table.iter().enumerate() {
-                        if !entry.is_unused() {
-                            println!("L2 Entry {}: {:?}", i, entry);
-
-                            let phys = entry.frame().unwrap().start_address();
-                            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-                            let ptr = VirtAddr::new(virt).as_mut_ptr();
-                            let l1_table: &PageTable = unsafe { &*ptr };
-
-                            for (i, entry) in l1_table.iter().enumerate() {
-                                if !entry.is_unused() {
-                                    println!("L1 Entry {}: {:?}", i, entry);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
