ecff72230f403f647c5994997a15eb3613bf65d9
gary.gan <garyglgan@gmail.com>
2020-04-14 22:49:12 +0800

    map zero to vga buffer

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use ggos::memory;
     use x86_64::{
-        structures::paging::{MapperAllSizes, PageTable},
+        structures::paging::{MapperAllSizes, Page, PageTable},
         VirtAddr,
     };
 
@@ -23,19 +23,14 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
 
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { mapper.translate_addr(virt) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,26 @@
 use x86_64::{
-    structures::paging::{OffsetPageTable, PageTable},
+    structures::paging::{
+        FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PageTableFlags, PhysFrame,
+        Size4KiB, UnusedPhysFrame,
+    },
     PhysAddr, VirtAddr,
 };
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+
+    let unused_frame = unsafe { UnusedPhysFrame::new(frame) };
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = mapper.map_to(page, unused_frame, flags, frame_allocator);
+    map_to_result.expect("map_to failed").flush();
+}
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
@@ -18,4 +37,12 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<UnusedPhysFrame<Size4KiB>> {
+        None
+    }
 }
