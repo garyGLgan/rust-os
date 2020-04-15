0e96e35a9e96c46f66a00f2770a025f9c5e00417
gary.gan <garyglgan@gmail.com>
2020-04-15 20:44:37 +0800

    fix warning

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,17 @@
 #![reexport_test_harness_main = "test_main"]
 
 extern crate alloc;
+use alloc::boxed::Box;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use ggos::println;
-use alloc::boxed::Box;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use ggos::memory;
     use ggos::memory::BoolInfoFrameAllocator;
-    use x86_64::{
-        structures::paging::{MapperAllSizes, Page, PageTable},
-        VirtAddr,
-    };
+    use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
 
@@ -26,12 +23,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
 
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        BoolInfoFrameAllocator::init(&boot_info.memory_map)
-    };
+    let _mapper = unsafe { memory::init(phys_mem_offset) };
+    let _frame_allocator = unsafe { BoolInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let x = Box::new(41);
+    let _x = Box::new(41);
 
     #[cfg(test)]
     test_main();
