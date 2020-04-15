4d8e79ff39b29f1076fde5b2abbf6877c319ccb7
gary.gan <garyglgan@gmail.com>
2020-04-15 23:19:30 +0800

    add heap allocator

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 extern crate alloc;
-use alloc::boxed::Box;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use ggos::println;
@@ -13,6 +13,7 @@ use ggos::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use ggos::allocator;
     use ggos::memory;
     use ggos::memory::BoolInfoFrameAllocator;
     use x86_64::VirtAddr;
@@ -23,10 +24,31 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
 
-    let _mapper = unsafe { memory::init(phys_mem_offset) };
-    let _frame_allocator = unsafe { BoolInfoFrameAllocator::init(&boot_info.memory_map) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BoolInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let _x = Box::new(41);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
